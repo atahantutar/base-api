63e5ce1d0a31371ed28c23170139216173090394
atahantutar <atahantutar69@gmail.com>
2023-09-15 22:48:36 +0300

    feat: created user modal.

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("users", userSchema);
